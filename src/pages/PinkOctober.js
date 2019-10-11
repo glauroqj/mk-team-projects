a80c97ee7a11f6d310ea9b6449be9b531af216f1
glauroqj <glauro.juliani@hotmail.com>
2019-10-10 21:59:53 -0300

    add: slider

--- src/pages/PinkOctober.js
+++ src/pages/PinkOctober.js
@@ -1,32 +1,51 @@
 import React, { useState } from 'react'
 /** components */
+import { CarouselProvider, Slider, Slide, Image } from 'pure-react-carousel'
 import Loading from '../components/Loading'
 /** theme */
 import '../assets/css/themes/pink-october.scss'
+import 'pure-react-carousel/dist/react-carousel.es.css'
 
+import img1 from '../assets/images/pink-october/01.png'
+import img2 from '../assets/images/pink-october/02.png'
+import img3 from '../assets/images/pink-october/03.png'
+import img4 from '../assets/images/pink-october/04.png'
+import img5 from '../assets/images/pink-october/05.png'
 
 const PinkOctober = () => {
   const [state, setState] = useState({
-    loading: true
+    loading: false,
+    arrayImg: [
+      img1,
+      img2,
+      img3,
+      img4,
+      img5
+    ]
   })
 
   if (state.loading) return <Loading text='Carregando...' />
 
   return (
-    <div className="pink-october">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="pink-october animated fadeIn">
+      <CarouselProvider
+        naturalSlideWidth={100}
+        naturalSlideHeight={180}
+        currentSlide={0}
+        step={1}
+        dragStep={1}
+        touchEnabled={true}
+        totalSlides={state.arrayImg.length}
+        lockOnWindowScroll={true}
+      >
+        <Slider>
+          {state.arrayImg.map((item, index) => (
+            <Slide index={index}>
+              <Image key={index} className="img-slider-responsive" src={item} alt="outuro-rosa-imagens" />
+            </Slide>
+          ))}
+        </Slider>
+      </CarouselProvider>
     </div>
   )
 }
